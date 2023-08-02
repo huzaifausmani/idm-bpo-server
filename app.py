73069f89024c6de6df63073b2d2e5f943090c6b5
HuzaifaUsmani <huzaifa.1019@gmail.com>
2023-08-02 21:35:21 +0500

    admin_info, contact_info API

---
+++ app.py
@@ -0,0 +1,61 @@
+import json
+from flask_cors import CORS
+from flask import Flask, request, jsonify
+from flask_restx import Resource, Api
+from flask_pymongo import PyMongo
+from model import Model
+
+with open("config.json") as jsondata:
+    config = json.load(jsondata)
+
+app = Flask(__name__)
+app.config[
+    "MONGO_URI"
+] = f'mongodb+srv://{config["USER"]}:{config["PASS"]}@cluster0.xsrdmeh.mongodb.net/{config["DB"]}?retryWrites=true&w=majority'
+cors = CORS(app)
+app.config["SECRET_KEY"] = "your-secret-key"
+api = Api(app)
+mongo = PyMongo(app)
+model = Model()
+model.mongo = mongo.db
+model.initialize_collections()
+
+
+@api.route("/admin_info")
+class AdminInfo(Resource):
+    def post(self):
+        admin_info = {
+            "username": request.json["username"],
+            "password": request.json["password"],
+        }
+        if model.update_admin_info(admin_info):
+            return jsonify(
+                {"status": True, "message": "Admin info updated successfully"}
+            )
+        else:
+            return jsonify({"status": False, "error": "Admin info updation failed"})
+
+
+@api.route("/contact_info")
+class ContactInfo(Resource):
+    def post(self):
+        contact_info = {
+            "telephone": request.json["telephone"],
+            "mail": request.json["mail"],
+        }
+        if model.update_contact_info(contact_info):
+            return jsonify(
+                {"status": True, "message": "Contact info updated successfully"}
+            )
+        else:
+            return jsonify({"status": False, "error": "Contact info updation failed"})
+
+    def get(self):
+        contact_info = model.get_contact_info()
+        return jsonify(
+            {"telephone": contact_info["telephone"], "mail": contact_info["mail"]}
+        )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ model.py
@@ -0,0 +1,55 @@
+from bson import json_util, ObjectId
+import json
+
+
+class Model:
+    mongo = ""
+    comments = ""
+    admin = ""
+    contact_info = ""
+
+    def initialize_collections(self):
+        self.comments = self.mongo.comments
+        self.admin = self.mongo.admin
+        self.contact_info = self.mongo.contact_info
+
+    @staticmethod
+    def convert_bson_to_json(data):
+        return json.loads(json_util.dumps(data))
+
+    def get_all_comments(cls):
+        return cls.convert_bson_to_json(cls.comments.find())
+
+    def get_admin(cls):
+        return cls.convert_bson_to_json(cls.admin.find())
+
+    def get_contact_info(cls):
+        return cls.convert_bson_to_json(cls.contact_info.find())[0]
+
+    def add_comment(cls, comment):
+        cls.comments.insert_one(comment)
+        return True
+
+    def update_admin_info(cls, admin_info):
+        cls.admin.update_one(
+            {"_id": ObjectId("64ca82f82fe928929f9803e4")},
+            {
+                "$set": {
+                    "username": admin_info["username"],
+                    "password": admin_info["password"],
+                }
+            },
+        )
+        return True
+
+    def update_contact_info(cls, contact_info):
+        cls.contact_info.update_one(
+            {"_id": ObjectId("64c7cf8c3908f37c5a8f5ae1")},
+            {
+                "$set": {
+                    "mail": contact_info["mail"],
+                    "telephone": contact_info["telephone"],
+                }
+            },
+        )
+        return True
